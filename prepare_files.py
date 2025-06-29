21a9a2494d73aae92cb7602e9cb19f822a55ced8
Yousefabdelsalam <yousefabdelsalam92@gmail.com>
2025-06-29 10:35:58 +0300

    files added

--- prepare_files.py
+++ prepare_files.py
@@ -1,29 +1,18 @@
 import gdown
-import zipfile
 import os
 
-# ملفات Google Drive IDs
-FILES = [
-    {
-        "filename": "model_w2v.model",
-        "file_id": "1zKRiCSlb2V99xo97UXmJSyZ39omVK9D3",
-        "output": "model_w2v.model"
-    },
-    
-    {
-        "filename": "stremlit.csv",
-        "file_id": "1exwAtDvLgHthuHMUROr2OihQtgRC1B3Y",
-        "output": "stremlit.csv"
-    }
-]
+# === روابط Google Drive ===
+files = {
+    "model_w2v.model": "1zKRiCSlb2V99xo97UXmJSyZ39omVK9D3",
+    "StremlitClustring.csv": "1kiH5q2p3a9uKp2xc9V9zOuJjiYaAhcjT",
+    "streamlit.csv": "1exwAtDvLgHthuHMUROr2OihQtgRC1B3Y"
+}
 
-# تحميل الملفات الناقصة فقط
-for file in FILES:
-    if not os.path.exists(file["filename"]):
-        print(f"⬇️ Downloading {file['filename']}...")
-        url = f"https://drive.google.com/uc?id={file['file_id']}"
-        gdown.download(url, file['output'], quiet=False)
-        print(f"✅ Downloaded {file['filename']}")
+# === تحميل الملفات ===
+for filename, file_id in files.items():
+    if not os.path.exists(filename):
+        print(f"⬇️ Downloading {filename}...")
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, filename, quiet=False)
     else:
-        print(f"✅ {file['filename']} already exists.")
-
+        print(f"✅ {filename} already exists.")
